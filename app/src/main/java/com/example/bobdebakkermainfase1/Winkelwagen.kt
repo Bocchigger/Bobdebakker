9d8066f046db31850663e8638712c2211b0c458d
SamaKiwi2010 <joastapken@gmail.com>
2024-11-07 11:16:52 +0100

    "Selecteer een winkel" button als constrained layout toegevoegd.

--- app/src/main/java/com/example/bobdebakkermainfase1/Winkelwagen.kt
+++ app/src/main/java/com/example/bobdebakkermainfase1/Winkelwagen.kt
@@ -1,6 +1,8 @@
 package com.example.bobdebakkermainfase1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class Winkelwagen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var itemCount = findViewById<TextView>(R.id.itemCount)
+        findViewById<Button>(R.id.plusBtn).setOnClickListener {
+            itemCount.text = (itemCount.text.toString().toInt() + 1).toString()
         }
     }
 }

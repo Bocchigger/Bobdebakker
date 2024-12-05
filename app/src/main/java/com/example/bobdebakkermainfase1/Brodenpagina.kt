df4e9085ca1eefa4eb985112f455febeb9306600
Bocchigger <liamzoer@gmail.com>
2024-12-05 10:58:51 +0100

    logo toegevoegd, navbar met placeholder icons.

--- app/src/main/java/com/example/bobdebakkermainfase1/Brodenpagina.kt
+++ app/src/main/java/com/example/bobdebakkermainfase1/Brodenpagina.kt
@@ -1,31 +1,20 @@
 package com.example.bobdebakkermainfase1
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Brodenpagina : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_brodenpagina)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginpagina)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        findViewById<Button>(R.id.Bigbroodbutton).setOnClickListener {
-            var intent = Intent(this, Brodenpagina::class.java)
-            startActivity(intent)
-        }
-        findViewById<Button>(R.id.Bigaccountbutton).setOnClickListener {
-            var intent = Intent(this, Loginpagina::class.java)
-            startActivity(intent)
         }
     }
 }
